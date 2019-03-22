3bfd8f334af5549408066d8207cbf76f26566ac1
colombelli <xcolombelli@gmail.com>
2019-03-22 08:55:53 -0300

    Improved NN

---
+++ Second NN - ReLU Softmax Cross_Entropy/testing_network.py
@@ -0,0 +1,65 @@
+from neural_network import *
+import pickle
+import os
+import tensorflow as tf
+
+
+# function that clears the console output
+def clear_output():
+    if os.name == 'nt':  # for windows
+        _ = os.system('cls')
+    else: # for mac and linux
+        _ = os.system('clear')
+
+
+# function that prints the parcial results on console
+def print_results(sample_num, label, nn_guess, matches, loss):
+    clear_output()
+    print("Sample %d | Label = %d | Output = %d | %d matches" % (sample_num, label, nn_guess, matches))
+    print("Loss: ", loss)
+    print("Hit rate: %.2f%%" % (matches / sample_num * 100))
+    print("\n")
+
+
+filename = input("File name: ")  # asks for user to input the .bin file with the containing the trained network
+while (not os.path.exists(filename)):  # keeps asking for the file name if the given one doesn't exists
+    filename = input("Please, give an existing file name with its extension: ")
+
+with open(filename,'rb') as openfile:  # after validating that the file exists in the directory, opens it in read mode
+    network = pickle.load(openfile)
+
+
+# loads the dataset
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+sample_num = 0
+matches = 0
+total_loss = 0
+MAX_TEST_SAMPLES = 10000  # all test samples in the dataset
+for sample, label in zip(x_test, y_test):
+
+    sample_num += 1
+    sample = np.concatenate(sample)/255
+
+    # convert the output to one hot encoded
+    expected_output = np.zeros(10)
+    expected_output[label] = 1
+
+    network_output = network.feedForward(sample)  # computes network output
+    loss = network.cross_entropy_cost_function(expected_output, network_output)  # computes loss
+    total_loss += loss  # sums up the loss for calculating the final average loss
+
+    nn_guess = np.argmax(network_output)  # gets the index of the highest value in the array
+    if nn_guess == label:  # check if the network got a right guess
+        matches += 1
+
+    if (sample_num % 100) == 0:  # prints parcial results from 100 to 100 tested samples
+        print_results(sample_num, label, nn_guess, matches, loss)
+
+    if sample_num == MAX_TEST_SAMPLES:
+      break
+
+
+print("\nAverage loss: %f" %(total_loss / sample_num))
+print("Final hit rate: %.2f%%" % (matches / sample_num * 100))
---
+++ Second NN - ReLU Softmax Cross_Entropy/training_network.py
@@ -0,0 +1,98 @@
+from neural_network import *
+import pickle
+import os
+import tensorflow as tf
+import time
+
+
+# function that clears the console output
+def clear_output():
+    if os.name == 'nt':  # for windows
+        _ = os.system('cls')
+    else: # for mac and linux
+        _ = os.system('clear')
+
+
+# function that saves a given neural network object into a binary file
+def save_nn(filename, nn, time_taken, samples):
+
+    # builds a string responsible for logs into a text file reporting the time taken for train that amount of samples
+    stringReport = str(samples) + " samples | time " + str(time_taken) + " minutes\n"
+
+    with (open(filename, 'wb')) as openfile:  # saves the trained neural network
+        pickle.dump(nn, openfile)
+    with (open("reports.txt", 'a')) as openfile:  # saves the report string without overwriting, that's way we open it with the append (a) mode
+        openfile.write(stringReport)
+
+
+# loads the dataset
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+
+# Training a network with the following structure
+# 784 neurons for input
+# 85 neurons for the hidden layer
+# 10 neurons for output (representing the numbers from 0 to 9)
+numInputs = 784
+neuronsEachLayer = [85, 10]
+network = NeuralNetwork(numInputs, neuronsEachLayer)
+learning_rate = 0.0001
+repeatTrainSamples = 2
+MAX_TRAINING_SAMPLES = 100000 #len(y_train) * repeatTrainSamples  # samples: 840.000
+
+sample_n = 0
+matches = 0
+total_loss = 0
+startTime = time.time()
+
+for i in range(repeatTrainSamples):  # iterates through the 60k train samples more than once
+
+    for sample, label in zip(x_train, y_train):
+        sample_n += 1
+
+        if (sample_n % 500) == 0:
+            # prints status of the training
+            time_taken = (time.time() - startTime) / 60
+            clear_output()
+            print("%d samples out of %d" %(sample_n, MAX_TRAINING_SAMPLES))
+            print("%.2f minutes used" %time_taken)  # prints how much minutes were already used to train the nn
+
+            # saves intermidiate trained networks
+            if sample_n == 10000:
+                save_nn("nn_10k.bin", network, time_taken, sample_n)
+
+            elif sample_n == 20000:
+                save_nn("nn_20k.bin", network, time_taken, sample_n)
+
+            elif sample_n == 50000:
+                save_nn("nn_50k.bin", network, time_taken, sample_n)
+
+            elif sample_n == 70000:
+                save_nn("nn_70k.bin", network, time_taken, sample_n)
+
+            elif sample_n == 100000:
+                save_nn("nn_100k.bin", network, time_taken, sample_n)
+
+        sample = np.concatenate(sample)/255  # shapes the sample in the format of an array of 784 lenght also normalizing it
+        sample = np.array(sample, dtype=np.float64)  # avoids the possible overflow about to come with the exp function
+
+        # convert the output to one hot encoded
+        expected_output = np.zeros(10)
+        expected_output[label] = 1
+        network_output = network.feedForward(sample)  # computes network output
+
+        nn_guess = np.argmax(network_output)  # gets the index of the highest value in the array
+        if nn_guess == label:  # check if the network got a right
+            matches += 1
+
+        # update weights and biases
+        network.backpropagation(expected_output, network_output, learning_rate)
+
+        # checks if it needs to continue the loop
+        if sample_n == MAX_TRAINING_SAMPLES:
+            time_taken = time.time() - startTime
+            break
+
+print("Time taken: ", time_taken)
+print("\n\n")
